2f001bb5d9e7b8ca779c344a268399b837464017
Spiros Raptis <spirosrap@gmail.com>
2018-03-10 00:10:02 +0200

    valid_actions

--- FCND-Motion-Planning/planning_utils.py
+++ FCND-Motion-Planning/planning_utils.py
@@ -92,13 +92,13 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
-    if (x - 1 < 0 and y - 1 < 0) or grid[x - 1, y - 1] == 1:
+    if (x - 1 < 0 or y - 1 < 0) or grid[x - 1, y - 1] == 1:
         valid_actions.remove(Action.NORTHWEST)
-    if (x + 1 > n and y - 1 < 0) or grid[x + 1, y - 1] == 1:
+    if (x + 1 > n or y - 1 < 0) or grid[x + 1, y - 1] == 1:
         valid_actions.remove(Action.SOUTHWEST)
-    if (x - 1 < 0 and y + 1 > m) or grid[x - 1, y + 1] == 1:
+    if (x - 1 < 0 or y + 1 > m) or grid[x - 1, y + 1] == 1:
         valid_actions.remove(Action.NORTHEAST)
-    if (x + 1 > n and y + 1 > m) or grid[x + 1, y + 1] == 1:
+    if (x + 1 > n or y + 1 > m) or grid[x + 1, y + 1] == 1:
         valid_actions.remove(Action.SOUTHEAST)
 
     return valid_actions

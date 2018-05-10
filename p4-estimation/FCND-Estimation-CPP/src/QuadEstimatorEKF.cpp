819df972087ea860d3efc77876850adc5aa98178
Spiros Raptis <spirosrap@gmail.com>
2018-05-10 12:50:25 +0300

    step2

--- p4-estimation/FCND-Estimation-CPP/src/QuadEstimatorEKF.cpp
+++ p4-estimation/FCND-Estimation-CPP/src/QuadEstimatorEKF.cpp
@@ -93,14 +93,26 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+//  float predictedPitch = pitchEst + dtIMU * gyro.y;
+//  float predictedRoll = rollEst + dtIMU * gyro.x;
+//  ekfState(6) = ekfState(6) + dtIMU * gyro.z;    // yaw
+//
+//  // normalize yaw to -pi .. pi
+//  if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
+//  if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
 
-  // normalize yaw to -pi .. pi
-  if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
-  if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+  Quaternion<float> qt = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+//
+//    Quaternion<float> dq = Quaternion<float>::FromEuler123_RPY(dtIMU *gyro.x,dtIMU *gyro.y,dtIMU *gyro.z);
+//
+//  Quaternion<float> qt_hat = dq * qt;
+//  float predictedRoll = qt_hat.Roll();
+//  float predictedPitch = qt_hat.Pitch();
 
+//    Mat3x3F R = qt.RotationMatrix_IwrtB()
+
+
+    
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   // CALCULATE UPDATE
